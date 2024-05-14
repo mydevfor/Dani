0b65cf40b72e2ae1d68e7c1e7e23530d0c00a7bc
mydevfor <156622575+mydevfor@users.noreply.github.com>
2024-05-13 22:17:42 -0400

    Add files via upload

---
+++ js-javascript.js
@@ -0,0 +1,10 @@
+function enforceMinMax(el) {
+    if (el.value != "") {
+        if (parseInt(el.value) < parseInt(el.min)) {
+            el.value = el.min;
+        }
+        if (parseInt(el.value) > parseInt(el.max)) {
+            el.value = el.max;
+        }
+    }
+}
